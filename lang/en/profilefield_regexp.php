63936d970f2302b5fab3ce79b1e05300e8a87907
Enrico Zamprogno <enricozamprogno@users.noreply.github.com>
2014-05-07 16:04:40 +0200

    Create profilefield_regexp.php

---
+++ lang/en/profilefield_regexp.php
@@ -0,0 +1,9 @@
+/**
+ * Strings for component 'profilefield_regexp', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   profilefield_regexp
+ * @copyright 2014 onwards Enrico Zamprogno
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Regexp Match Text input';
